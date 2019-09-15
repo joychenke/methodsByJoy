a5dafdf6b9dd5408ebbc13bae5ac0c9bb49f45c2
chen ke <joyyouxiang@163.com>
2019-09-15 13:48:35 +0800

    module加载实现

---
+++ js现有方法/module模块化编程/module的加载实现 _eg.js
@@ -0,0 +1,14 @@
+// 浏览器使用 type="module",加载ES6模块
+<script type="module" src="module.js"></script>
+// 用上面的方式加载ES6的模块，相当于加上了defer属性
+
+// 内嵌网页中
+/* 
+<script type="module">
+  import utils from "./utils.js"
+</script>
+*/
+
+/* 
+// 
+*/
