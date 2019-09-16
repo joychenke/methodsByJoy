f1bc279e2aa76137772f3270c67e59c44d2cbf23
chen ke <joyyouxiang@163.com>
2019-09-16 23:51:13 +0800

    加入module

--- js现有方法/module模块化编程/module的加载实现 _eg.js
+++ js现有方法/module模块化编程/module的加载实现 _eg.js
@@ -1,14 +1,30 @@
 // 浏览器使用 type="module",加载ES6模块
-<script type="module" src="module.js"></script>
+/*<script type="module" src="module.js"></script>
 // 用上面的方式加载ES6的模块，相当于加上了defer属性
 
 // 内嵌网页中
-/* 
 <script type="module">
+ 
   import utils from "./utils.js"
 </script>
-*/
 
-/* 
-// 
+*//* 
+
+// CommonJS 和 ES6的两种等效写法
+module.exports = {
+  foo: "hello",
+}
+bar: "world"
+等效于
+export default {
+  foo: "hello",
+  bar: "world"
+}
+用import命令加载上面的模块时，实际上输入的是这样的对象
+
+{ default: module.exports}
+
+一共有三种写法，可以拿到 CommonJS 模块的module.exports
 */
+CommonJS
+ES6
--- js现有方法/module模块化编程/module的加载实现.js
+++ js现有方法/module模块化编程/module的加载实现.js
@@ -34,8 +34,56 @@ export通过接口，输出的是同一个值，不同脚本加载这个接口�
 /* 
 3, node加载
 Node的import目前只支持加载本地模块，不支持加载远程模块
+
 如果模块名不含路径，那么import命令回去node_modules目录找，比如  
 import "baz"
 import Router from "vue-router"
-如果模块名包含路径，那么会取;
+
+如果模块名包含路径，那么会按照路径去寻找这个名字的脚本文件
+
+如果脚本文件省略了后缀名 比如import './foo' ，会依次做下列尝试：
+./foo.mjs、./foo.js、./foo.json、./foo.node  =》  ./foo/package.json的main字段指定的脚本  =》./foo/index.mjs、./foo/index.js、./foo/index.json、./foo/index.node  以上三层，都没找到时，会抛出错误。
+
+Node 的import命令是异步加载
 */
+
+/* 
+内部变量
+ES6中顶层的this指向undefined，CommonJS中指向的是当前模块。以下变量都是不存在的
+arguments , require , module , exports , __filename , __dirname
+*/
+
+/* 
+ES6模块加载CommonJS模块
+CommonJS 模块的输出都定义在module.exports属性上面。
+CommonJS和ES6的两种等效写法（看例子）
+*/
+
+/* 
+CommonJS模块加载ES6模块
+CommonJS 模块加载 ES6 模块，不能使用require命令，而要使用import()函数 （详情看文档）
+*/
+
+/* 
+4，循环加载
+循环加载指的是 a循环依赖b，b循环依赖a
+
+CommonJS模块加载原理：
+require命令第一次加载该脚本，就会执行整个脚本，然后在内存生成一个对象。无论加载多少次，都只会在第一次加载时运行一次。以后再加载，都只会返回第一次运行的结果，除非手动清除系统缓存
+
+CommonJS模块的循环加载：
+CommonJS 模块遇到循环加载时，返回的是当前已经执行的部分的值，而不是代码全部执行后的值（看文档例子代码）
+
+ES6模块的循环加载：
+解决ES6不能直接循环加载的问题。可以将变量写成函数解决。这是利用里函数具有提升作用。（看懂ES6循环加载那几个例子）
+*/
+
+/* 
+5，ES6模块的转码
+Babel：浏览器还不支持ES6，用Babel可以将ES6转成 ES5 的写法。
+还有其他两个方法，也可以用来转码。
+ES6 module transpiler
+SystemJS   使用方法： ①载入system.js ②System.import()方法加载模块文件，它返回一个 Promise 对象
+*/
+CommonJS
+ES6
