1bb8838274c8122eac4ecd24140fa522fd312a6b
chen ke <joyyouxiang@163.com>
2020-01-01 23:14:51 +0800

    修改文件名

--- js现有方法/字符串/substring.js
+++ js现有方法/字符串/substring.js
@@ -1,5 +1,5 @@
 // 不要用substr，用substring代替
 let str = "I have a dream, that one day I'll be a singer."
 let newStr = str.substring()
-let newStr1 = str.substring(0,1)
+let newStr1 = str.substring(0,3)
 console.log(newStr1);
